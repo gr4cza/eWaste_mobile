af851916b92eeed0d91bef4bcab9214986110740
Dávid Gracza <graczadavid@gmail.com>
2021-11-15 23:56:06 +0100

    Change Object tracking from MLKit to MediaPipe

--- app/src/main/java/hu/bme/ewaste/ui/MainActivity.kt
+++ app/src/main/java/hu/bme/ewaste/ui/MainActivity.kt
@@ -1,24 +1,27 @@
 package hu.bme.ewaste.ui
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
 import android.os.Bundle
 import android.util.Size
-import android.widget.Toast
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import com.google.mediapipe.components.CameraHelper.CameraFacing
+import com.google.mediapipe.components.CameraXPreviewHelper
+import com.google.mediapipe.components.ExternalTextureConverter
+import com.google.mediapipe.components.FrameProcessor
+import com.google.mediapipe.components.PermissionHelper
+import com.google.mediapipe.formats.proto.DetectionProto.Detection
+import com.google.mediapipe.framework.AndroidAssetUtil
+import com.google.mediapipe.framework.Packet
+import com.google.mediapipe.framework.PacketGetter
+import com.google.mediapipe.glutil.EglManager
 import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.ewaste.R
 import hu.bme.ewaste.databinding.ActivityMainBinding
 import timber.log.Timber
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -27,31 +30,64 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var cameraExecutor: ExecutorService
+    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
+    private lateinit var previewFrameTexture: SurfaceTexture
 
-    private val targetSize = Size(480, 640)
+    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
+    private lateinit var previewDisplayView: SurfaceView
+
+    // Creates and manages an {@link EGLContext}.
+    private var eglManager: EglManager = EglManager(null)
+
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    private lateinit var processor: FrameProcessor
+
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private lateinit var converter: ExternalTextureConverter
+
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    private lateinit var cameraHelper: CameraXPreviewHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
+        previewDisplayView = SurfaceView(this)
+        setupPreviewDisplayView()
 
-        // Request camera permissions
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
 
-        viewModel.detectedObjects.observe(this) { detectedObjects ->
-            binding.boundingBoxes.detectedObjects = detectedObjects
-        }
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this)
 
-        binding.boundingBoxes.apply {
-            imageWidth = targetSize.width
-            imageHeight = targetSize.height
+        eglManager = EglManager(null)
+        processor = FrameProcessor(
+            this,
+            eglManager.nativeContext,
+            BINARY_GRAPH_NAME,
+            INPUT_VIDEO_STREAM_NAME,
+            OUTPUT_VIDEO_STREAM_NAME
+        )
+        processor.videoSurfaceOutput.setFlipY(FLIP_FRAMES_VERTICALLY)
+
+        PermissionHelper.checkAndRequestCameraPermissions(this)
+
+        processor.addPacketCallback(
+            OUTPUT_TRACKED_DETECTIONS_STREAM_NAME
+        ) { packet: Packet? ->
+            val detectedObjects =
+                PacketGetter.getProtoVector(
+                    packet,
+                    Detection.parser()
+                )
+            for (detectedObject in detectedObjects) {
+                Timber.d(detectedObject.toString())
+            }
         }
 
         viewModel.isTracking.observe(this) { tracking ->
@@ -67,74 +103,107 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+    override fun onResume() {
+        super.onResume()
+        converter = ExternalTextureConverter(eglManager.context).apply {
+            setFlipY(FLIP_FRAMES_VERTICALLY)
+            setConsumer(processor)
+        }
 
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-                }
-
-            val objectDetector = ImageAnalysis.Builder()
-                .setTargetResolution(targetSize)
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, viewModel.trashCanObjectDetector)
-                }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, objectDetector
-                )
-            } catch (e: Exception) {
-                Timber.e(e, "Use case binding failed")
-            }
-
-        }, ContextCompat.getMainExecutor(this))
-    }
-
-
-    // Manage Permissions
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(
-                    this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
-            }
+        if (PermissionHelper.cameraPermissionsGranted(this)) {
+            startCamera()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
+    override fun onPause() {
+        super.onPause()
+        converter.close()
+    }
+
+    private fun startCamera() {
+        cameraHelper = CameraXPreviewHelper()
+        cameraHelper.setOnCameraStartedListener { surfaceTexture ->
+            if (surfaceTexture != null) {
+                previewFrameTexture = surfaceTexture
+            }
+            // Make the display view visible to start showing the preview. This triggers the
+            // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+            previewDisplayView.visibility = View.VISIBLE
+        }
+        cameraHelper.startCamera(this, CAMERA_FACING,  /*surfaceTexture=*/null)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun setupPreviewDisplayView() {
+        previewDisplayView.visibility = View.GONE
+        val viewGroup = binding.previewDisplayLayout
+        viewGroup.addView(previewDisplayView)
+        previewDisplayView
+            .holder
+            .addCallback(
+                object : SurfaceHolder.Callback {
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        processor.videoSurfaceOutput.setSurface(holder.surface)
+                    }
+
+                    override fun surfaceChanged(
+                        holder: SurfaceHolder,
+                        format: Int,
+                        width: Int,
+                        height: Int
+                    ) {
+                        onPreviewDisplaySurfaceChanged(width, height)
+                    }
+
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        processor.videoSurfaceOutput.setSurface(null)
+                    }
+                })
+    }
+
+    private fun onPreviewDisplaySurfaceChanged(
+        width: Int, height: Int
+    ) {
+        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+        // based on the size of the SurfaceView that contains the display.
+        val viewSize = Size(width, height)
+        val displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize)
+        val isCameraRotated = cameraHelper.isCameraRotated
+
+        // Connect the converter to the camera-preview frames as its input (via
+        // previewFrameTexture), and configure the output width and height as the computed
+        // display size.
+        converter.setSurfaceTextureAndAttachToGLContext(
+            previewFrameTexture,
+            if (isCameraRotated) displaySize.height else displaySize.width,
+            if (isCameraRotated) displaySize.width else displaySize.height
+        )
     }
 
     companion object {
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            arrayOf(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION)
+        init {
+            // Load all native libraries needed by the app.
+            System.loadLibrary("mediapipe_jni")
+            System.loadLibrary("opencv_java3")
+        }
+
+        private const val BINARY_GRAPH_NAME = "mobile_gpu.binarypb"
+        private const val INPUT_VIDEO_STREAM_NAME = "input_video"
+        private const val OUTPUT_VIDEO_STREAM_NAME = "output_video"
+        private const val OUTPUT_TRACKED_DETECTIONS_STREAM_NAME = "tracked_detections"
+        private val CAMERA_FACING = CameraFacing.BACK
+
+        // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
+        // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
+        // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
+        // corner, whereas MediaPipe in general assumes the image origin is at top-left.
+        private const val FLIP_FRAMES_VERTICALLY = true
     }
 }
