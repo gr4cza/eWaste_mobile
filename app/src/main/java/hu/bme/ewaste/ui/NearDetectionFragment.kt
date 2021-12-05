ca1f1d026c78d7a310c08fbac660c63a192e1252
Dávid Gracza <graczadavid@gmail.com>
2021-12-05 11:15:40 +0100

    Basic location in NearDetectionsViewModel.kt

--- app/src/main/java/hu/bme/ewaste/ui/NearDetectionFragment.kt
+++ app/src/main/java/hu/bme/ewaste/ui/NearDetectionFragment.kt
@@ -1,5 +1,7 @@
 package hu.bme.ewaste.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,5 +11,10 @@ import hu.bme.ewaste.R
 class NearDetectionFragment : Fragment(R.layout.fragment_near_detections) {
 
     private val viewModel by viewModels<NearDetectionsViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.toString()
+    }
 
 }
--- app/src/main/java/hu/bme/ewaste/ui/NearDetectionsViewModel.kt
+++ app/src/main/java/hu/bme/ewaste/ui/NearDetectionsViewModel.kt
@@ -1,11 +1,75 @@
 package hu.bme.ewaste.ui
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationRequest
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.tasks.CancellationTokenSource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import hu.bme.ewaste.repository.TrashCanRepository
+import hu.bme.ewaste.service.TrashCanTracker
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class NearDetectionsViewModel @Inject constructor(
+    private val trashCanRepository: TrashCanRepository,
+    private val fusedLocationClient: FusedLocationProviderClient,
+    @ApplicationContext appContext: Context
 ) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var cancellationTokenSource: CancellationTokenSource = CancellationTokenSource()
+
+    init {
+        Timber.d("NearDetectionsViewModel started!")
+
+        viewModelScope.launch {
+            if (locationPermissionsGranted(appContext)) {
+                while (isActive) {
+                    val location = getLocation()
+                    val nearDetectedTrashCans = trashCanRepository.getNearDetectedTrashCans(
+                        location = hu.bme.ewaste.data.dto.Location(
+                            location.latitude,
+                            location.longitude
+                        )
+                    )
+                    Timber.d(nearDetectedTrashCans.toString())
+                    delay(5000L)
+                }
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission", "InlinedApi")
+    private suspend fun getLocation(): Location {
+        return fusedLocationClient.getCurrentLocation(
+            LocationRequest.QUALITY_HIGH_ACCURACY,
+            cancellationTokenSource.token
+        ).await()
+    }
+
+    private fun locationPermissionsGranted(appContext: Context): Boolean {
+        TrashCanTracker.LOCATION_PERMISSIONS.forEach {
+            if (ActivityCompat.checkSelfPermission(
+                    appContext,
+                    it
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
 }
